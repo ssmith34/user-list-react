357db3df2c06ae6cf63ab4c27072bb1d24d73a63
Shane <slsmith0906@gmail.com>
2023-08-17 18:55:27 -0400

    Finished updates

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,17 +2,27 @@ import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
 import { useState } from 'react';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [errorMessage, setErrorMessage] = useState();
     
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setErrorMessage({
+                title: 'Invalid Username',
+                message: 'Please enter a valid name and age (non-empty values).'
+            })
             return;
         }
         if (+enteredAge < 1) {
+            setErrorMessage({
+                title: 'Invalid Age',
+                message: 'Please enter a valid age (must be greater than 0).'
+            })
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -28,7 +38,15 @@ const AddUser = props => {
         setEnteredAge(event.target.value);
     };
 
+    const errorMessageHandler = () => {
+        setErrorMessage(null);
+    }
     return (
+        <>
+        {errorMessage && <ErrorModal 
+            title={errorMessage.title} 
+            message={errorMessage.message}
+            onAcknowledge={errorMessageHandler}/>}
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label 
@@ -57,6 +75,7 @@ const AddUser = props => {
                 </Button>
             </form>
         </Card>
+        </>
     )
 };
 
